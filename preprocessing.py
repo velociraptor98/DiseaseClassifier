cb6145ef599e70a2f49a1c4b0a5b120b4aaf1383
Bosu sai Sree Ram Charan.K <48154493+BosuSai@users.noreply.github.com>
2019-09-02 10:47:21 +0530

    Obtaining the training data and converting characters of it to UTF-8 standard and creating English Stopwords and Lemmatizer objects

--- preprocessing.py
+++ preprocessing.py
@@ -17,3 +17,9 @@ file = open('training_text', 'r', encoding='utf-8')
 training_text = file.read()
 train_list = training_text.split('\n')
 train_list = train_list[1:len(train_list)]
+
+train_csv=csv.writer(open('train_csv.csv','w',encoding = 'UTF-8')) # Inorder to convert all chacarters to UTF-8 standard
+train_csv.writerow(['ID','Text'])
+
+stopwords = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
